181d6a95968f2fe82fa53dc0ef8ab4fdaf9483d9
Jesalim <jessyculture@gmail.com>
2023-02-11 13:09:34 +0300

    anagram-detector

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :word, :match
+
+   def initialize (word)
+    @agram = word
+    @word= word.chars.sort
+    return @word
+  
+   end
+
+   def match gram
+    
+ #   @gram = gram.map {|l|  l.chars.sort}
+    
+   @new =  gram.select {|words| words.chars.sort == @word}
+   if @new
+    return @new
+   else
+    return[]
+   end
+   end
+end
+
+l = Anagram.new('mondya')
+l.match(%W[tuesday monday,wednesday,fiday])
